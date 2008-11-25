f8d9601195bbb05ed70b6b6f45b7c655774f0070
Dan Armstrong <dan@aoindustries.com>
2008-11-25 23:25:43 +0000

    Made note to move to NOC

--- src/com/aoindustries/aoserv/daemon/monitor/NetStat.java
+++ src/com/aoindustries/aoserv/daemon/monitor/NetStat.java
@@ -6,9 +6,10 @@ package com.aoindustries.aoserv.daemon.monitor;
  * All rights reserved.
  */
 import com.aoindustries.aoserv.client.IPAddress;
-import com.aoindustries.profiler.Profiler;
 
 /**
+ * TODO: Move this to NOC
+ *
  * @author  AO Industries, Inc.
  */
 final public class NetStat {
@@ -18,94 +19,71 @@ final public class NetStat {
     public int port;
 
     public NetStat(String net_protocol, String ip_address, int port) {
-        Profiler.startProfile(Profiler.INSTANTANEOUS, NetStat.class, "<init>(String,String,int)", null);
-        try {
-            this.net_protocol=net_protocol;
-            this.ip_address=ip_address;
-            this.port=port;
-        } finally {
-            Profiler.endProfile(Profiler.INSTANTANEOUS);
-        }
+        this.net_protocol=net_protocol;
+        this.ip_address=ip_address;
+        this.port=port;
     }
 
+    @Override
     public boolean equals(Object O) {
-        Profiler.startProfile(Profiler.FAST, NetStat.class, "equals(Object)", null);
-        try {
-            if(O instanceof NetStat) {
-                NetStat ns=(NetStat)O;
-                return
-                    net_protocol.equals(ns.net_protocol)
-                    && ip_address.equals(ns.ip_address)
-                    && port==ns.port
-                ;
-            } else return false;
-        } finally {
-            Profiler.endProfile(Profiler.FAST);
-        }
+        if(O instanceof NetStat) {
+            NetStat ns=(NetStat)O;
+            return
+                net_protocol.equals(ns.net_protocol)
+                && ip_address.equals(ns.ip_address)
+                && port==ns.port
+            ;
+        } else return false;
     }
 
+    @Override
     public int hashCode() {
-        Profiler.startProfile(Profiler.FAST, NetStat.class, "hashCode()", null);
-        try {
-            return ip_address.hashCode()+port;
-        } finally {
-            Profiler.endProfile(Profiler.FAST);
-        }
+        return ip_address.hashCode()+port;
     }
 
     public static NetStat parseNetStat(String line) {
-        Profiler.startProfile(Profiler.UNKNOWN, NetStat.class, "parseNetStat(String)", null);
-        try {
-            int len=line.length();
+        int len=line.length();
 
-            // find the first word
-            int pos=line.indexOf(' ');
-            if(pos==-1) return null;
-            String protocol=line.substring(0,pos);
-            if(
-                protocol.equals("Active")
-                || protocol.equals("Proto")
-                || protocol.equals("unix")
-            ) return null;
+        // find the first word
+        int pos=line.indexOf(' ');
+        if(pos==-1) return null;
+        String protocol=line.substring(0,pos);
+        if(
+            protocol.equals("Active")
+            || protocol.equals("Proto")
+            || protocol.equals("unix")
+        ) return null;
 
-            /*
-             * Find the fourth word, which is the IP address/port combination
-             */
-            // skip past first blank
-            while((++pos)<len && line.charAt(pos)==' ');
-            // skip past second word
-            pos=line.indexOf(' ', pos);
-            if(pos==-1) return null;
-            // skip past second blank
-            while((++pos)<len && line.charAt(pos)==' ');
-            // skip past third word
-            pos=line.indexOf(' ', pos);
-            if(pos==-1) return null;
-            // skip past th-ee-ald blank
-            while((++pos)<len && line.charAt(pos)==' ');
-            // Find the end of the IP:port column
-            int pos2=line.indexOf(' ', pos+1);
-            if(pos2==-1) pos2=len;
-            // Find the last : in the IP:port area
-            int colonPos=line.lastIndexOf(':', pos2);
-            if(colonPos==-1) return null;
-            String ip=convertIP(line.substring(pos, colonPos));
-            int port=Integer.parseInt(line.substring(colonPos+1, pos2));
-            return new NetStat(protocol, ip, port);
-        } finally {
-            Profiler.endProfile(Profiler.UNKNOWN);
-        }
+        /*
+         * Find the fourth word, which is the IP address/port combination
+         */
+        // skip past first blank
+        while((++pos)<len && line.charAt(pos)==' ');
+        // skip past second word
+        pos=line.indexOf(' ', pos);
+        if(pos==-1) return null;
+        // skip past second blank
+        while((++pos)<len && line.charAt(pos)==' ');
+        // skip past third word
+        pos=line.indexOf(' ', pos);
+        if(pos==-1) return null;
+        // skip past th-ee-ald blank
+        while((++pos)<len && line.charAt(pos)==' ');
+        // Find the end of the IP:port column
+        int pos2=line.indexOf(' ', pos+1);
+        if(pos2==-1) pos2=len;
+        // Find the last : in the IP:port area
+        int colonPos=line.lastIndexOf(':', pos2);
+        if(colonPos==-1) return null;
+        String ip=convertIP(line.substring(pos, colonPos));
+        int port=Integer.parseInt(line.substring(colonPos+1, pos2));
+        return new NetStat(protocol, ip, port);
     }
 
     private static String convertIP(String ip) {
-        Profiler.startProfile(Profiler.FAST, NetStat.class, "convertIP()", null);
-        try {
-            if("::ffff:127.0.0.1".equals(ip)) return IPAddress.LOOPBACK_IP;
-            if("::".equals(ip)) return IPAddress.WILDCARD_IP;
-            if("::1".equals(ip)) return IPAddress.LOOPBACK_IP;
-            return ip;
-        } finally {
-            Profiler.endProfile(Profiler.FAST);
-        }
+        if("::ffff:127.0.0.1".equals(ip)) return IPAddress.LOOPBACK_IP;
+        if("::".equals(ip)) return IPAddress.WILDCARD_IP;
+        if("::1".equals(ip)) return IPAddress.LOOPBACK_IP;
+        return ip;
     }
 }
--- src/com/aoindustries/aoserv/daemon/monitor/NetStatMonitor.java
+++ src/com/aoindustries/aoserv/daemon/monitor/NetStatMonitor.java
@@ -5,15 +5,21 @@ package com.aoindustries.aoserv.daemon.monitor;
  * 7262 Bull Pen Cir, Mobile, Alabama, 36695, U.S.A.
  * All rights reserved.
  */
-import com.aoindustries.aoserv.client.*;
-import com.aoindustries.aoserv.daemon.*;
-import com.aoindustries.profiler.*;
-import com.aoindustries.util.*;
-import java.io.*;
-import java.sql.*;
-import java.util.*;
+import com.aoindustries.aoserv.client.NetBind;
+import com.aoindustries.aoserv.client.NetProtocol;
+import com.aoindustries.aoserv.daemon.AOServDaemon;
+import com.aoindustries.aoserv.daemon.AOServDaemonConfiguration;
+import com.aoindustries.util.SortedArrayList;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
+ * TODO: Move this to NOC
+ *
  * The configuration for all AOServ processes is stored in a properties file.
  *
  * @author  AO Industries, Inc.
@@ -40,146 +46,127 @@ final public class NetStatMonitor implements Runnable {
     private static Thread thread;
     
     private NetStatMonitor() {
-        Profiler.startProfile(Profiler.INSTANTANEOUS, NetStatMonitor.class, "<init>()", null);
-        Profiler.endProfile(Profiler.INSTANTANEOUS);
     }
     
     private static int getRandomDelay() {
-        Profiler.startProfile(Profiler.UNKNOWN, NetStatMonitor.class, "getRandomDelay()", null);
-        try {
-            return MIN_DELAY+AOServDaemon.getRandom().nextInt(MAX_DELAY-MIN_DELAY);
-        } finally {
-            Profiler.endProfile(Profiler.UNKNOWN);
-        }
+        return MIN_DELAY+AOServDaemon.getRandom().nextInt(MAX_DELAY-MIN_DELAY);
     }
     
     public void run() {
-        Profiler.startProfile(Profiler.UNKNOWN, NetStatMonitor.class, "run()", null);
-        try {
-            while(true) {
-                try {
-                    while(true) {
-                        try {
-                            Thread.sleep(getRandomDelay());
-                        } catch(InterruptedException err) {
-                            AOServDaemon.reportWarning(err, null);
-                        }
-                        
-                        // Get the current list of binds for this server
-                        List<NetBind> nbs=AOServDaemon.getThisAOServer().getServer().getNetBinds();
-                        
-                        // Build a ArrayList that will be trimmed as matches are found
-                        List<NetBind> dbBinds=new ArrayList<NetBind>(nbs.size());
-                        for(int c=0;c<nbs.size();c++) dbBinds.add(nbs.get(c));
-                        
-                        // Keep track of net binds that are returned by netstat but not in dbBinds
-                        List<NetStat> extraBinds=new ArrayList<NetStat>();
-                        
-                        // Parse through each net bind returned by netstat
-                        Process P=Runtime.getRuntime().exec(netstat);
-                        try {
-                            P.getOutputStream().close();
-                            BufferedReader in=new BufferedReader(new InputStreamReader(P.getInputStream()));
-                            try {
-                                List<NetStat> allNetStats=new SortedArrayList<NetStat>();
-                                String line;
-                                while((line=in.readLine())!=null) {
-                                    NetStat netStat=NetStat.parseNetStat(line);
-                                    if(netStat!=null) {
-                                        // Skip port 1 raw
-                                        if(netStat.port!=1 || !netStat.net_protocol.equals(NetProtocol.RAW)) {
-                                            // Must be unique to continue
-                                            if(!allNetStats.contains(netStat)) {
-                                                allNetStats.add(netStat);
-                                                // Find one or more matches and remove, otherwise add to extra bind info
-                                                boolean found=false;
-                                                for(int c=0;c<dbBinds.size();c++) {
-                                                    NetBind nb=dbBinds.get(c);
-                                                    if(
-                                                       nb.getIPAddress().getIPAddress().equals(netStat.ip_address)
-                                                       && nb.getNetProtocol().getProtocol().equals(netStat.net_protocol)
-                                                       && nb.getPort().getPort()==netStat.port
-                                                       ) {
-                                                        dbBinds.remove(c);
-                                                        c--;
-                                                        found=true;
-                                                    }
-                                                }
-                                                if(!found) extraBinds.add(netStat);
-                                            }
-                                        }
-                                    }
-                                }
-                            } finally {
-                                in.close();
-                            }
-                        } finally {
-                            int retCode=P.waitFor();
-                            if(retCode!=0) throw new IOException("Non-zero return code from /bin/netstat: "+retCode);
-                        }
-                        
-                        // Report and discrepancy
-                        int missing=0;
-                        for(int c=0;c<dbBinds.size();c++) {
-                            NetBind dbBind=dbBinds.get(c);
-                            if(dbBind.isMonitoringEnabled()) missing++;
-                        }
-                        int extra=extraBinds.size();
-                        if(missing!=0 || extra!=0) {
-                            StringBuilder SB=new StringBuilder();
-                            SB.append("Discrepancies found in netstat -ln:");
-                            if(missing!=0) SB.append(" missing ").append(missing).append(missing==1?" net bind":" net binds");
-                            if(extra!=0) {
-                                if(missing!=0) SB.append(" and");
-                                SB.append(" found ").append(extra).append(" extra net ").append(extra==1?"bind":"binds");
-                            }
-                            SB.append('\n');
-                            for(int c=0;c<dbBinds.size();c++) {
-                                NetBind nb=dbBinds.get(c);
-                                if(nb.isMonitoringEnabled()) {
-                                    SB.append("Missing: ").append(nb.getNetProtocol()).append(' ').append(nb.getIPAddress().getIPAddress()).append(':').append(nb.getPort()).append('\n');
-                                }
-                            }
-                            for(int c=0;c<extra;c++) {
-                                NetStat ns=extraBinds.get(c);
-                                SB.append("Extra: ").append(ns.net_protocol).append(' ').append(ns.ip_address).append(':').append(ns.port).append('\n');
-                            }
-                            AOServDaemon.reportFullMonitoringMessage(SB.toString());
-                        }
-                    }
-                } catch(ThreadDeath TD) {
-                    throw TD;
-                } catch(Throwable T) {
-                    AOServDaemon.reportError(T, null);
+        while(true) {
+            try {
+                while(true) {
                     try {
-                        Thread.sleep(Math.max(MIN_ERROR_DELAY, getRandomDelay()));
+                        Thread.sleep(getRandomDelay());
                     } catch(InterruptedException err) {
                         AOServDaemon.reportWarning(err, null);
                     }
+
+                    // Get the current list of binds for this server
+                    List<NetBind> nbs=AOServDaemon.getThisAOServer().getServer().getNetBinds();
+
+                    // Build a ArrayList that will be trimmed as matches are found
+                    List<NetBind> dbBinds=new ArrayList<NetBind>(nbs.size());
+                    for(int c=0;c<nbs.size();c++) dbBinds.add(nbs.get(c));
+
+                    // Keep track of net binds that are returned by netstat but not in dbBinds
+                    List<NetStat> extraBinds=new ArrayList<NetStat>();
+
+                    // Parse through each net bind returned by netstat
+                    Process P=Runtime.getRuntime().exec(netstat);
+                    try {
+                        P.getOutputStream().close();
+                        BufferedReader in=new BufferedReader(new InputStreamReader(P.getInputStream()));
+                        try {
+                            List<NetStat> allNetStats=new SortedArrayList<NetStat>();
+                            String line;
+                            while((line=in.readLine())!=null) {
+                                NetStat netStat=NetStat.parseNetStat(line);
+                                if(netStat!=null) {
+                                    // Skip port 1 raw
+                                    if(netStat.port!=1 || !netStat.net_protocol.equals(NetProtocol.RAW)) {
+                                        // Must be unique to continue
+                                        if(!allNetStats.contains(netStat)) {
+                                            allNetStats.add(netStat);
+                                            // Find one or more matches and remove, otherwise add to extra bind info
+                                            boolean found=false;
+                                            for(int c=0;c<dbBinds.size();c++) {
+                                                NetBind nb=dbBinds.get(c);
+                                                if(
+                                                   nb.getIPAddress().getIPAddress().equals(netStat.ip_address)
+                                                   && nb.getNetProtocol().getProtocol().equals(netStat.net_protocol)
+                                                   && nb.getPort().getPort()==netStat.port
+                                                   ) {
+                                                    dbBinds.remove(c);
+                                                    c--;
+                                                    found=true;
+                                                }
+                                            }
+                                            if(!found) extraBinds.add(netStat);
+                                        }
+                                    }
+                                }
+                            }
+                        } finally {
+                            in.close();
+                        }
+                    } finally {
+                        int retCode=P.waitFor();
+                        if(retCode!=0) throw new IOException("Non-zero return code from /bin/netstat: "+retCode);
+                    }
+
+                    // Report and discrepancy
+                    int missing=0;
+                    for(int c=0;c<dbBinds.size();c++) {
+                        NetBind dbBind=dbBinds.get(c);
+                        if(dbBind.isMonitoringEnabled()) missing++;
+                    }
+                    int extra=extraBinds.size();
+                    if(missing!=0 || extra!=0) {
+                        StringBuilder SB=new StringBuilder();
+                        SB.append("Discrepancies found in netstat -ln:");
+                        if(missing!=0) SB.append(" missing ").append(missing).append(missing==1?" net bind":" net binds");
+                        if(extra!=0) {
+                            if(missing!=0) SB.append(" and");
+                            SB.append(" found ").append(extra).append(" extra net ").append(extra==1?"bind":"binds");
+                        }
+                        SB.append('\n');
+                        for(int c=0;c<dbBinds.size();c++) {
+                            NetBind nb=dbBinds.get(c);
+                            if(nb.isMonitoringEnabled()) {
+                                SB.append("Missing: ").append(nb.getNetProtocol()).append(' ').append(nb.getIPAddress().getIPAddress()).append(':').append(nb.getPort()).append('\n');
+                            }
+                        }
+                        for(int c=0;c<extra;c++) {
+                            NetStat ns=extraBinds.get(c);
+                            SB.append("Extra: ").append(ns.net_protocol).append(' ').append(ns.ip_address).append(':').append(ns.port).append('\n');
+                        }
+                        AOServDaemon.reportFullMonitoringMessage(SB.toString());
+                    }
+                }
+            } catch(ThreadDeath TD) {
+                throw TD;
+            } catch(Throwable T) {
+                AOServDaemon.reportError(T, null);
+                try {
+                    Thread.sleep(Math.max(MIN_ERROR_DELAY, getRandomDelay()));
+                } catch(InterruptedException err) {
+                    AOServDaemon.reportWarning(err, null);
                 }
             }
-        } finally {
-            Profiler.endProfile(Profiler.UNKNOWN);
         }
     }
     
     public static void start() throws IOException, SQLException {
-        Profiler.startProfile(Profiler.UNKNOWN, NetStatMonitor.class, "start()", null);
-        try {
-            // Avoid netstat monitoring when nested or in failover mode
-            if(AOServDaemonConfiguration.isManagerEnabled(NetStatMonitor.class) && !AOServDaemonConfiguration.isNested() && AOServDaemon.getThisAOServer().getFailoverServer()==null) {
+        // Avoid netstat monitoring when nested or in failover mode
+        if(AOServDaemonConfiguration.isManagerEnabled(NetStatMonitor.class) && !AOServDaemonConfiguration.isNested() && AOServDaemon.getThisAOServer().getFailoverServer()==null) {
+            synchronized(System.out) {
                 if(thread==null) {
-                    synchronized(System.out) {
-                        if(thread==null) {
-                            System.out.print("Starting NetStatMonitor: ");
-                            (thread=new Thread(new NetStatMonitor())).start();
-                            System.out.println("Done");
-                        }
-                    }
+                    System.out.print("Starting NetStatMonitor: ");
+                    (thread=new Thread(new NetStatMonitor())).start();
+                    System.out.println("Done");
                 }
             }
-        } finally {
-            Profiler.endProfile(Profiler.UNKNOWN);
         }
     }
 }
