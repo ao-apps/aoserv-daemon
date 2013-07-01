e4b2cfef591f3a9c7064742318cbab9022156106
Dan Armstrong <dan@aoindustries.com>
2013-07-01 12:12:37 +0000

    Auto null route.

---
+++ src/com/aoindustries/aoserv/daemon/monitor/NetworkMonitor.java
@@ -0,0 +1,530 @@
+/*
+ * Copyright 2013 by AO Industries, Inc.,
+ * 7262 Bull Pen Cir, Mobile, Alabama, 36695, U.S.A.
+ * All rights reserved.
+ */
+package com.aoindustries.aoserv.daemon.monitor;
+
+import com.aoindustries.aoserv.client.IPAddress;
+import com.aoindustries.aoserv.daemon.AOServDaemonConfiguration;
+import com.aoindustries.aoserv.daemon.LogFactory;
+import com.aoindustries.aoserv.daemon.net.NullRouteManager;
+import com.aoindustries.math.SafeMath;
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.DataInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintStream;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.logging.Level;
+
+/**
+ * Monitoring network traffic using the ip_counts command.
+ * The set of networks is configured in the aoserv-daemon.properties file.
+ *
+ * @author  AO Industries, Inc.
+ */
+final public class NetworkMonitor {
+
+	private static final boolean DEBUG = false;
+
+	private static final int MICROS_PER_SECOND = 1000000;
+
+	private static final Map<String,NetworkMonitor> inMonitors = new LinkedHashMap<String,NetworkMonitor>();
+	private static final Map<String,NetworkMonitor> outMonitors = new LinkedHashMap<String,NetworkMonitor>();
+
+	public static void start() throws IOException {
+        if(AOServDaemonConfiguration.isManagerEnabled(NetworkMonitor.class)) {
+			for(AOServDaemonConfiguration.NetworkMonitorConfiguration config : AOServDaemonConfiguration.getNetworkMonitors().values()) {
+				final String networkName = config.getName();
+				synchronized(System.out) {
+					if(!inMonitors.containsKey(networkName)) {
+						System.out.print("Starting NetworkMonitor(" + networkName +", in): ");
+						NetworkMonitor monitor = new NetworkMonitor(
+							config.getDevice(),
+							"in",
+							config.getNetworkRanges(),
+							config.getInNetworkDirection(),
+							config.getInCountDirection(),
+							config.getNullRoutePacketRate(),
+							config.getNullRouteBitRate()
+						);
+						inMonitors.put(networkName, monitor);
+						monitor.startThread();
+						System.out.println("Done");
+					}
+					if(!outMonitors.containsKey(networkName)) {
+						System.out.print("Starting NetworkMonitor(" + networkName +", out): ");
+						NetworkMonitor monitor = new NetworkMonitor(
+							config.getDevice(),
+							"out",
+							config.getNetworkRanges(),
+							config.getOutNetworkDirection(),
+							config.getOutCountDirection(),
+							null, // Null routes only done on incoming traffic
+							null  // Null routes only done on incoming traffic
+						);
+						outMonitors.put(networkName, monitor);
+						monitor.startThread();
+						System.out.println("Done");
+					}
+				}
+			}
+        }
+    }
+
+	private final String device;
+	private final String direction;
+	private final List<String> networkRanges;
+	private final AOServDaemonConfiguration.NetworkMonitorConfiguration.NetworkDirection networkDirection;
+	private final AOServDaemonConfiguration.NetworkMonitorConfiguration.CountDirection countDirection;
+	private final Long nullRoutePacketRate;
+	private final Long nullRouteBitRate;
+
+	private Thread thread;
+
+	private NetworkMonitor(
+		String device,
+		String direction,
+		List<String> networkRanges,
+		AOServDaemonConfiguration.NetworkMonitorConfiguration.NetworkDirection networkDirection,
+		AOServDaemonConfiguration.NetworkMonitorConfiguration.CountDirection countDirection,
+		Long nullRoutePacketRate,
+		Long nullRouteBitRate
+	) {
+		this.device = device;
+		this.direction = direction;
+		this.networkRanges = networkRanges;
+		this.networkDirection = networkDirection;
+		this.countDirection = countDirection;
+		this.nullRoutePacketRate = nullRoutePacketRate;
+		this.nullRouteBitRate = nullRouteBitRate;
+    }
+
+	static class Counts {
+		final long packets;
+		final long bytes;
+		Counts(long packets, long bytes) {
+			this.packets = packets;
+			this.bytes   = bytes;
+		}
+	}
+
+	private static final Counts ZERO_COUNTS = new Counts(0, 0);
+
+	private Counts readCounts(DataInputStream in) throws IOException {
+		final long packets = in.readLong();
+		final long bytes   = in.readLong();
+		if(
+			packets  == 0
+			&& bytes == 0
+		) return ZERO_COUNTS;
+		return new Counts(
+			packets,
+			bytes
+		);
+	}
+
+	static class ProtocolCounts {
+		final long icmpPackets;
+		final long icmpBytes;
+		final long udpPackets;
+		final long udpBytes;
+		final long tcpPackets;
+		final long tcpBytes;
+		final long otherPackets;
+		final long otherBytes;
+		ProtocolCounts(
+			long icmpPackets,
+			long icmpBytes,
+			long udpPackets,
+			long udpBytes,
+			long tcpPackets,
+			long tcpBytes,
+			long otherPackets,
+			long otherBytes
+		) {
+			this.icmpPackets  = icmpPackets;
+			this.icmpBytes    = icmpBytes;
+			this.udpPackets   = udpPackets;
+			this.udpBytes     = udpBytes;
+			this.tcpPackets   = tcpPackets;
+			this.tcpBytes     = tcpBytes;
+			this.otherPackets = otherPackets;
+			this.otherBytes   = otherBytes;
+		}
+
+		/**
+		 * Gets the packet count for all protocols combined.
+		 */
+		long getPackets() {
+			return icmpPackets + udpPackets + tcpPackets + otherPackets;
+		}
+
+		/**
+		 * Gets the byte count for all protocols combined.
+		 */
+		long getBytes() {
+			return icmpBytes + udpBytes + tcpBytes + otherBytes;
+		}
+	}
+
+	private static final ProtocolCounts ZERO_PROTOCOL_COUNTS = new ProtocolCounts(0, 0, 0, 0, 0, 0, 0, 0);
+
+	private ProtocolCounts readProtocolCounts(DataInputStream in) throws IOException {
+		final long icmpPackets  = in.readLong();
+		final long icmpBytes    = in.readLong();
+		final long udpPackets   = in.readLong();
+		final long udpBytes     = in.readLong();
+		final long tcpPackets   = in.readLong();
+		final long tcpBytes     = in.readLong();
+		final long otherPackets = in.readLong();
+		final long otherBytes   = in.readLong();
+		if(
+			icmpPackets     == 0
+			&& icmpBytes    == 0
+			&& udpPackets   == 0
+			&& udpBytes     == 0
+			&& tcpPackets   == 0
+			&& tcpBytes     == 0
+			&& otherPackets == 0
+			&& otherBytes   == 0
+		) return ZERO_PROTOCOL_COUNTS;
+		return new ProtocolCounts(
+			icmpPackets,
+			icmpBytes,
+			udpPackets,
+			udpBytes,
+			tcpPackets,
+			tcpBytes,
+			otherPackets,
+			otherBytes
+		);
+	}
+
+	abstract static class Network {
+		Network() {
+		}
+	}
+
+	static class IPv4Network extends Network {
+		final int address;
+		final byte prefix;
+		final ProtocolCounts totalCounts;
+		final ProtocolCounts[] ips;
+		IPv4Network(
+			int address,
+			byte prefix,
+			ProtocolCounts totalCounts,
+			ProtocolCounts[] ips
+		) {
+			this.address = address;
+			this.prefix = prefix;
+			this.totalCounts = totalCounts;
+			this.ips = ips;
+		}
+	}
+
+	private static void printTime(long seconds, int micros, PrintStream out) {
+		assert micros>=0 && micros <= 999999;
+		out.print(seconds);
+		out.print('.');
+		if(micros < 10) out.print("00000");
+		else if(micros < 100) out.print("0000");
+		else if(micros < 1000) out.print("000");
+		else if(micros < 10000) out.print("00");
+		else if(micros < 100000) out.print('0');
+		out.print(micros);
+	}
+
+	private static void printCounts(Counts counts, PrintStream out) {
+		out.print(counts.packets);
+		out.print('/');
+		out.print(counts.bytes);
+	}
+
+	private static void printProtocolCounts(String name, ProtocolCounts counts, PrintStream out) {
+		out.print(name);
+		out.print(".icmp=");
+		out.print(counts.icmpPackets);
+		out.print('/');
+		out.print(counts.icmpBytes);
+		out.println();
+		out.print(name);
+		out.print(".udp=");
+		out.print(counts.udpPackets);
+		out.print('/');
+		out.print(counts.udpBytes);
+		out.println();
+		out.print(name);
+		out.print(".tcp=");
+		out.print(counts.tcpPackets);
+		out.print('/');
+		out.print(counts.tcpBytes);
+		out.println();
+		out.print(name);
+		out.print(".other=");
+		out.print(counts.otherPackets);
+		out.print('/');
+		out.print(counts.otherBytes);
+		out.println();
+	}
+
+	private static long getBitRate(long bytes, long timeSpanMicros) {
+		return SafeMath.multiply(bytes, 8 * MICROS_PER_SECOND) / timeSpanMicros;
+	}
+
+	private static long getPacketRate(long packets, long timeSpanMicros) {
+		return SafeMath.multiply(packets, MICROS_PER_SECOND) / timeSpanMicros;
+	}
+
+	synchronized private void startThread() {
+		if(thread==null) {
+			final String threadName = NetworkMonitor.class.getName()+"("+device+", "+direction+")";
+			final String errorInThreadName = threadName+".errorIn";
+			final boolean controllingNullRoutes = nullRoutePacketRate!=null || nullRouteBitRate!=null;
+			thread = new Thread(threadName) {
+				@Override
+				public void run() {
+					while(true) {
+						try {
+							final String[] cmd = new String[
+								(controllingNullRoutes ? 0 : 1)
+								+ 6
+								+ networkRanges.size()
+							];
+							int index = 0;
+							// No nice level when controlling null routes
+							if(!controllingNullRoutes) cmd[index++] = "/bin/nice";
+							cmd[index++] = "/opt/aoserv-daemon/bin/ip_counts";
+							cmd[index++] = "1";
+							cmd[index++] = "binary";
+							cmd[index++] = device;
+							cmd[index++] = networkDirection.name();
+							cmd[index++] = countDirection.name();
+							for(String networkRange : networkRanges) cmd[index++] = networkRange;
+							assert index == cmd.length;
+
+							final Process process = Runtime.getRuntime().exec(cmd);
+							try {
+								// Log all warnings from ip_counts to stderr
+								final InputStream errorIn = process.getErrorStream();
+								new Thread(errorInThreadName) {
+									@Override
+									public void run() {
+										try {
+											BufferedReader in = new BufferedReader(new InputStreamReader(errorIn));
+											try {
+												String line;
+												while((line = in.readLine()) != null) {
+													synchronized(System.err) {
+														System.err.print(threadName);
+														System.err.print(": ");
+														System.err.println(line);
+													}
+												}
+											} finally {
+												in.close();
+											}
+										} catch (ThreadDeath TD) {
+											throw TD;
+										} catch (Throwable T) {
+											LogFactory.getLogger(NetworkMonitor.class).log(Level.SEVERE, null, T);
+										}
+									}
+								}.start();
+								// Read standard in
+								DataInputStream in = new DataInputStream(new BufferedInputStream(process.getInputStream()));
+								try {
+									while(true) {
+										// Read one record
+										final byte protocolVersion = in.readByte();
+										if(protocolVersion != 1) throw new IOException("Unexpected protocolVersion: "+protocolVersion);
+										final long timeStartSeconds = in.readLong();
+										final int timeStartMicros = in.readInt();
+										final long timeEndSeconds = in.readLong();
+										final int timeEndMicros = in.readInt();
+										final long ifaceDropped = in.readLong();
+										final long ifaceErrors = in.readLong();
+										final long ifaceFifoErrors = in.readLong();
+										final long pcapReceived = in.readLong();
+										final long pcapDropped = in.readLong();
+										final Counts totalIface = readCounts(in);
+										final Counts totalCaptured = readCounts(in);
+										final Counts totalExtrapolated = readCounts(in);
+										final Counts unparseable = readCounts(in);
+										final ProtocolCounts otherNetwork = readProtocolCounts(in);
+										final int numNetworks = in.readInt();
+										final Network[] networks = new Network[numNetworks];
+										for(int netIndex=0; netIndex<numNetworks; netIndex++) {
+											final byte ipVersion = in.readByte();
+											if(ipVersion == 4) {
+												final int address = in.readInt();
+												final byte prefix = in.readByte();
+												final ProtocolCounts totalCounts = readProtocolCounts(in);
+												final int numIps = 1 << (32 - prefix);
+												final ProtocolCounts[] ips = new ProtocolCounts[numIps];
+												for(int ipIndex=0; ipIndex<numIps; ipIndex++) {
+													ips[ipIndex] = readProtocolCounts(in);
+												}
+												networks[netIndex] = new IPv4Network(address, prefix, totalCounts, ips);
+											} else {
+												throw new IOException("Unexpected ipVersion: " + ipVersion);
+											}
+										}
+										if(DEBUG) {
+											final PrintStream out = System.out;
+											synchronized(out) {
+												out.print(threadName);
+												out.println(": Got report");
+												out.print("time.start=");
+												printTime(timeStartSeconds, timeStartMicros, out);
+												out.println();
+												out.print("time.end=");
+												printTime(timeEndSeconds, timeEndMicros, out);
+												out.println();
+												out.print("iface.dropped=");
+												out.println(ifaceDropped);
+												out.print("iface.errors=");
+												out.println(ifaceErrors);
+												out.print("iface.fifo_errors=");
+												out.println(ifaceFifoErrors);
+												out.print("pcap.received=");
+												out.println(pcapReceived);
+												out.print("pcap.dropped=");
+												out.println(pcapDropped);
+												out.print("total.iface=");
+												printCounts(totalIface, out);
+												out.println();
+												out.print("total.captured=");
+												printCounts(totalCaptured, out);
+												out.println();
+												out.print("total.extrapolated=");
+												printCounts(totalExtrapolated, out);
+												out.println();
+												out.print("unparseable=");
+												printCounts(unparseable, out);
+												out.println();
+												printProtocolCounts("other_network", otherNetwork, out);
+											}
+										}
+										// Compute time span of last sample in microseconds
+										long timeSpanMicros = (timeEndSeconds - timeStartSeconds) * MICROS_PER_SECOND + (timeEndMicros - timeStartMicros);
+										// Do not null route on short samples
+										if(timeSpanMicros >= (MICROS_PER_SECOND/2)) {
+											// Add null routes due to packets/sec
+											if(nullRoutePacketRate != null) {
+												// Compute current packets/sec
+												long packetRate = getPacketRate(totalIface.packets, timeSpanMicros);
+												if(packetRate >= nullRoutePacketRate) {
+													// Find host with highest packets count (all protocols combined)
+													int nullingIp = 0;
+													long highestPacketCount = Long.MIN_VALUE;
+													for(Network network : networks) {
+														if(network instanceof IPv4Network) {
+															IPv4Network ipv4Network = (IPv4Network)network;
+															ProtocolCounts[] ips = ipv4Network.ips;
+															for(int ipIndex=0, len=ips.length; ipIndex<len; ipIndex++) {
+																long packetCount = ips[ipIndex].getPackets();
+																if(packetCount > highestPacketCount) {
+																	nullingIp = ipv4Network.address + ipIndex;
+																	highestPacketCount = packetCount;
+																}
+															}
+														} else {
+															throw new AssertionError("Unexpected type of network: " + network.getClass().getName());
+														}
+													}
+													if(highestPacketCount == Long.MIN_VALUE) throw new AssertionError("Unable to find IP to null route");
+													NullRouteManager.addNullRoute(nullingIp);
+													PrintStream out = System.out;
+													synchronized(out) {
+														out.print(threadName);
+														out.print(": null routing: ");
+														out.print(packetRate);
+														out.print(" pps >= ");
+														out.print(nullRoutePacketRate);
+														out.print(" pps: Found highest IP ");
+														out.print(IPAddress.getIPAddressForInt(nullingIp));
+														out.print(" @ ");
+														out.print(getPacketRate(highestPacketCount, timeSpanMicros));
+														out.println(" pps");
+													}
+												}
+											}
+											// Add null routes due to bits/sec
+											if(nullRouteBitRate != null) {
+												// Compute current bits/sec
+												long bitRate = getBitRate(totalIface.bytes, timeSpanMicros);
+												if(bitRate >= nullRouteBitRate) {
+													// Find host with highest bytes count (all protocols combined)
+													int nullingIp = 0;
+													long highestByteCount = Long.MIN_VALUE;
+													for(Network network : networks) {
+														if(network instanceof IPv4Network) {
+															IPv4Network ipv4Network = (IPv4Network)network;
+															ProtocolCounts[] ips = ipv4Network.ips;
+															for(int ipIndex=0, len=ips.length; ipIndex<len; ipIndex++) {
+																long byteCount = ips[ipIndex].getBytes();
+																if(byteCount > highestByteCount) {
+																	nullingIp = ipv4Network.address + ipIndex;
+																	highestByteCount = byteCount;
+																}
+															}
+														} else {
+															throw new AssertionError("Unexpected type of network: " + network.getClass().getName());
+														}
+													}
+													if(highestByteCount == Long.MIN_VALUE) throw new AssertionError("Unable to find IP to null route");
+													NullRouteManager.addNullRoute(nullingIp);
+													PrintStream out = System.out;
+													synchronized(out) {
+														out.print(threadName);
+														out.print(": null routing: ");
+														out.print(bitRate);
+														out.print(" bps >= ");
+														out.print(nullRouteBitRate);
+														out.print(" bps: Found highest IP ");
+														out.print(IPAddress.getIPAddressForInt(nullingIp));
+														out.print(" @ ");
+														out.print(getBitRate(highestByteCount, timeSpanMicros));
+														out.println(" bps");
+													}
+												}
+											}
+											// TODO: Add null route on fifo errors and/or iface drops?
+										}
+										// TODO: Build statistics database/history (in master since roles can change?  Or history per gateway?)
+										// TODO: Report to any listeners
+									}
+								} finally {
+									in.close();
+								}
+							} finally {
+								int retVal = process.waitFor();
+								if(retVal!=0) throw new IOException("non-zero exit code: " + retVal);
+							}
+						} catch (ThreadDeath TD) {
+							throw TD;
+						} catch (Throwable T) {
+							LogFactory.getLogger(NetworkMonitor.class).log(Level.SEVERE, null, T);
+						}
+						try {
+							// Sleep less on error when controlling null routes
+							sleep(controllingNullRoutes ? 1000 : 10000);
+						} catch (InterruptedException err) {
+							LogFactory.getLogger(NetworkMonitor.class).log(Level.WARNING, null, err);
+						}
+					}
+				}
+			};
+			// Maximum priority when controlling null routes, normal priority otherwise
+			thread.setPriority(controllingNullRoutes ? Thread.MAX_PRIORITY : Thread.NORM_PRIORITY);
+			thread.start();
+		}
+	}
+}
---
+++ src/com/aoindustries/aoserv/daemon/net/NullRouteManager.java
@@ -0,0 +1,267 @@
+/*
+ * Copyright 2013 by AO Industries, Inc.,
+ * 7262 Bull Pen Cir, Mobile, Alabama, 36695, U.S.A.
+ * All rights reserved.
+ */
+package com.aoindustries.aoserv.daemon.net;
+
+import com.aoindustries.aoserv.client.IPAddress;
+import com.aoindustries.aoserv.client.validator.InetAddress;
+import com.aoindustries.aoserv.daemon.AOServDaemonConfiguration;
+import com.aoindustries.aoserv.daemon.LogFactory;
+import com.aoindustries.aoserv.daemon.server.VirtualServerManager;
+import com.aoindustries.aoserv.daemon.unix.linux.LinuxProcess;
+import com.aoindustries.io.FileUtils;
+import com.aoindustries.nio.charset.Charset;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.PrintStream;
+import java.util.Iterator;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.logging.Level;
+
+/**
+ * Manages the null route config file.
+ *
+ * @author  AO Industries, Inc.
+ */
+final public class NullRouteManager {
+
+	private static final boolean DEBUG = true;
+
+	/**
+	 * When an IP address is repeatedly null routed, its duration is increased.
+	 */
+	private static final long[] durations = {
+		1L * 60L * 1000L, // 1 minute
+		2L * 60L * 1000L, // 2 minutes
+		5L * 60L * 1000L, // 5 minutes
+		10L * 60L * 1000L, // 10 minutes
+	};
+
+	/**
+	 * A null route level is reduced only after no null route for this time span.
+	 */
+	private static final long REDUCE_LEVEL_QUIET_TIME = 60L * 60L * 1000L; // 1 hour
+
+	private static final File BIRD_NULL_CONFIG = new File("/etc/opt/aoserv-daemon/bird-null-auto.conf");
+	private static final File BIRD_NULL_CONFIG_NEW = new File("/etc/opt/aoserv-daemon/bird-null-auto.conf.new");
+
+	volatile private static NullRouteManager instance;
+
+	public static void start() throws IOException {
+        if(AOServDaemonConfiguration.isManagerEnabled(NullRouteManager.class)) {
+			synchronized(System.out) {
+				if(instance==null) {
+					System.out.print("Starting NullRouteManager: ");
+					instance = new NullRouteManager();
+					instance.startThread();
+					System.out.println("Done");
+				}
+			}
+        }
+    }
+
+	static class NullRoute {
+		final int ip;
+		int level;
+		long startTime;
+		long endTime;
+
+		NullRoute(int ip, long currentTime) {
+			this.ip = ip;
+			this.level = 0;
+			this.startTime = currentTime;
+			this.endTime = currentTime + durations[level];
+			if(DEBUG) {
+				final PrintStream out = System.out;
+				synchronized(out) {
+					out.print(NullRouteManager.class.getName());
+					out.print(IPAddress.getIPAddressForInt(ip));
+					out.print(": new: level=");
+					out.println(level);
+				}
+			}
+		}
+
+		/**
+		 * Decrement the null route level based on the amount of quiet time
+		 */
+		void reduceLevel(long currentTime) {
+			// Check for system time set to past
+			if(currentTime < startTime) {
+				// System time reset to the past, start over
+				level = -1;
+				if(DEBUG) {
+					final PrintStream out = System.out;
+					synchronized(out) {
+						out.print(NullRouteManager.class.getName());
+						out.print(IPAddress.getIPAddressForInt(ip));
+						out.print(": system time reset: level=");
+						out.println(level);
+					}
+				}
+			} else if(currentTime >= endTime) {
+				long decrementLevels = (currentTime - endTime) / REDUCE_LEVEL_QUIET_TIME;
+				assert decrementLevels >= 0;
+				if(decrementLevels != 0) {
+					level -= Math.min(decrementLevels, durations.length);
+					if(level < -1) level = -1;
+					if(DEBUG) {
+						final PrintStream out = System.out;
+						synchronized(out) {
+							out.print(NullRouteManager.class.getName());
+							out.print(IPAddress.getIPAddressForInt(ip));
+							out.print(": decremented: level=");
+							out.println(level);
+						}
+					}
+				}
+			}
+		}
+
+		/**
+		 * Increases the null route level and starts a new null route time period
+		 */
+		void increaseLevel(long currentTime) {
+			level = Math.min(level+1, durations.length-1);
+			startTime = currentTime;
+			endTime = currentTime + durations[level];
+			if(DEBUG) {
+				final PrintStream out = System.out;
+				synchronized(out) {
+					out.print(NullRouteManager.class.getName());
+					out.print(IPAddress.getIPAddressForInt(ip));
+					out.print(": incremented: level=");
+					out.println(level);
+				}
+			}
+		}
+	}
+
+	private static final Map<Integer,NullRoute> nullRoutes = new LinkedHashMap<Integer,NullRoute>();
+
+	private Thread thread;
+
+	private NullRouteManager() {
+    }
+
+	synchronized private void startThread() {
+		if(thread==null) {
+			thread = new Thread(NullRouteManager.class.getName()) {
+				@Override
+				public void run() {
+					while(true) {
+						try {
+							StringBuilder newContents = new StringBuilder();
+							synchronized(nullRoutes) {
+								while(true) {
+									// Verify config file while cleaning-up entries
+									newContents.setLength(0);
+									final long currentTime = System.currentTimeMillis();
+									long nearestEnding = Long.MAX_VALUE;
+									Iterator<Map.Entry<Integer,NullRoute>> iter = nullRoutes.entrySet().iterator();
+									while(iter.hasNext()) {
+										Map.Entry<Integer,NullRoute> entry = iter.next();
+										NullRoute nullRoute = entry.getValue();
+										// If null route currently in progress, add to the output file
+										if(currentTime >= nullRoute.startTime && currentTime < nullRoute.endTime) {
+											String ipString = IPAddress.getIPAddressForInt(nullRoute.ip);
+											InetAddress inetAddress = InetAddress.valueOf(ipString);
+											assert inetAddress.isIPv4();
+											// Never null-route private IP addresses, such as those used for communication between routers for BGP sessions
+											if(!inetAddress.isUniqueLocal()) {
+												newContents
+													.append("route ")
+													.append(ipString)
+													.append("/32 drop;\n")
+												;
+											}
+											// Find the null route that expires next
+											if(nullRoute.endTime < nearestEnding) nearestEnding = nullRoute.endTime;
+										} else {
+											nullRoute.reduceLevel(currentTime);
+											if(nullRoute.level < 0) {
+												// Quiet long enough to remove entirely
+												iter.remove();
+											}
+										}
+									}
+									byte[] newBytes = newContents.toString().getBytes(Charset.UTF_8.name()); // .name() only for JDK < 1.6 compatibility
+									// See if file has changed
+									if(!FileUtils.contentEquals(BIRD_NULL_CONFIG, newBytes)) {
+										// Write new file
+										OutputStream out = new FileOutputStream(BIRD_NULL_CONFIG_NEW);
+										try {
+											out.write(newBytes);
+										} finally {
+											out.close();
+										}
+										FileUtils.rename(BIRD_NULL_CONFIG_NEW, BIRD_NULL_CONFIG);
+										// kill -HUP bird if updated
+										int pid = VirtualServerManager.findPid("/opt/bird/sbin/bird\u0000-u\u0000bird\u0000-g\u0000bird");
+				                        if(pid == -1) {
+											LogFactory.getLogger(NullRouteManager.class).log(Level.SEVERE, "bird not running");
+										} else {
+											new LinuxProcess(pid).signal("HUP");
+										}
+									}
+									// Wait for this amount of time, or indefinitely if none
+									if(nearestEnding == Long.MAX_VALUE) {
+										nullRoutes.wait();
+									} else {
+										long waitTime = nearestEnding - System.currentTimeMillis();
+										if(waitTime>0) nullRoutes.wait(waitTime);
+									}
+								}
+							}
+						} catch (ThreadDeath TD) {
+							throw TD;
+						} catch (Throwable T) {
+							LogFactory.getLogger(NullRouteManager.class).log(Level.SEVERE, null, T);
+						}
+						try {
+							sleep(1000);
+						} catch (InterruptedException err) {
+							LogFactory.getLogger(NullRouteManager.class).log(Level.WARNING, null, err);
+						}
+					}
+				}
+			};
+			thread.setPriority(Thread.MAX_PRIORITY);
+			thread.start();
+		}
+	}
+
+	/**
+	 * Adds a new null route to the system.
+	 */
+	public static void addNullRoute(int nullingIp) {
+		final Integer ipObj = nullingIp;
+		final long currentTime = System.currentTimeMillis();
+		// Look for an existing null route
+		synchronized(nullRoutes) {
+			NullRoute nullRoute = nullRoutes.get(ipObj);
+			if(nullRoute!=null) {
+				// If null route currently in progress, ignore request
+				if(currentTime >= nullRoute.startTime && currentTime < nullRoute.endTime) {
+					// Ignore request
+					return;
+				}
+				nullRoute.reduceLevel(currentTime);
+				if(nullRoute.level < 0) {
+					// Quiet long enough to start over
+					nullRoute = null;
+				} else {
+					// Increase level
+					nullRoute.increaseLevel(currentTime);
+				}
+			}
+			if(nullRoute==null) nullRoutes.put(ipObj, new NullRoute(nullingIp, currentTime));
+			nullRoutes.notify();
+		}
+	}
+}
