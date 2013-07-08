8f98fa26796c880eaf5d21be7f3c93003fe66156
Dan Armstrong <dan@aoindustries.com>
2013-07-08 09:07:12 +0000

    Tomcat 7 support.

---
+++ src/com/aoindustries/aoserv/daemon/httpd/tomcat/HttpdTomcatSharedSiteManager_7_0_X.java
@@ -0,0 +1,93 @@
+/*
+ * Copyright 2013 by AO Industries, Inc.,
+ * 7262 Bull Pen Cir, Mobile, Alabama, 36695, U.S.A.
+ * All rights reserved.
+ */
+package com.aoindustries.aoserv.daemon.httpd.tomcat;
+
+import com.aoindustries.aoserv.client.HttpdTomcatContext;
+import com.aoindustries.aoserv.client.HttpdTomcatSharedSite;
+import com.aoindustries.aoserv.daemon.util.FileUtils;
+import com.aoindustries.io.ChainWriter;
+import com.aoindustries.io.unix.UnixFile;
+import java.io.BufferedOutputStream;
+import java.io.IOException;
+import java.sql.SQLException;
+
+/**
+ * Manages HttpdTomcatSharedSite version 6.0.X configurations.
+ *
+ * @author  AO Industries, Inc.
+ */
+class HttpdTomcatSharedSiteManager_7_0_X extends HttpdTomcatSharedSiteManager<TomcatCommon_7_0_X> {
+
+    HttpdTomcatSharedSiteManager_7_0_X(HttpdTomcatSharedSite tomcatSharedSite) throws SQLException, IOException {
+        super(tomcatSharedSite);
+    }
+
+    /**
+     * Builds a shared site for Tomcat 7.0.X
+     */
+    protected void buildSiteDirectoryContents(UnixFile siteDirectory) throws IOException, SQLException {
+        /*
+         * Resolve and allocate stuff used throughout the method
+         */
+        final String siteDir = siteDirectory.getPath();
+        final int uid = httpdSite.getLinuxServerAccount().getUid().getID();
+        final int gid = httpdSite.getLinuxServerGroup().getGid().getID();
+
+        /*
+         * Create the skeleton of the site, the directories and links.
+         */
+        FileUtils.mkdir(siteDir+"/bin", 0770, uid, gid);
+        FileUtils.mkdir(siteDir+"/conf", 0775, uid, gid);
+        FileUtils.mkdir(siteDir+"/daemon", 0770, uid, gid);
+        FileUtils.mkdir(siteDir+"/webapps", 0775, uid, gid);
+        FileUtils.mkdir(siteDir+"/webapps/"+HttpdTomcatContext.ROOT_DOC_BASE, 0775, uid, gid);
+        FileUtils.mkdir(siteDir+"/webapps/"+HttpdTomcatContext.ROOT_DOC_BASE+"/WEB-INF", 0775, uid, gid);
+        FileUtils.mkdir(siteDir+"/webapps/"+HttpdTomcatContext.ROOT_DOC_BASE+"/WEB-INF/classes", 0770, uid, gid);
+        FileUtils.mkdir(siteDir+"/webapps/"+HttpdTomcatContext.ROOT_DOC_BASE+"/WEB-INF/lib", 0770, uid, gid);
+
+        /*
+         * Write the ROOT/WEB-INF/web.xml file.
+         */
+        String webXML=siteDir+"/webapps/"+HttpdTomcatContext.ROOT_DOC_BASE+"/WEB-INF/web.xml";
+        ChainWriter out=new ChainWriter(
+            new BufferedOutputStream(
+                new UnixFile(webXML).getSecureOutputStream(uid, gid, 0664, false)
+            )
+        );
+        try {
+            out.print("<?xml version=\"1.0\" encoding=\"ISO-8859-1\"?>\n"
+                    + "<web-app xmlns=\"http://java.sun.com/xml/ns/javaee\"\n"
+                    + "  xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\"\n"
+                    + "  xsi:schemaLocation=\"http://java.sun.com/xml/ns/javaee\n"
+                    + "                      http://java.sun.com/xml/ns/javaee/web-app_3_0.xsd\"\n"
+                    + "  version=\"3.0\"\n"
+                    + "  metadata-complete=\"true\">\n"
+                    + "\n"
+                    + "  <display-name>Welcome to Tomcat</display-name>\n"
+                    + "  <description>\n"
+                    + "     Welcome to Tomcat\n"
+                    + "  </description>\n"
+                    + "\n"
+                    + "</web-app>\n");
+        } finally {
+            out.close();
+        }
+    }
+
+    public TomcatCommon_7_0_X getTomcatCommon() {
+        return TomcatCommon_7_0_X.getInstance();
+    }
+
+    protected boolean rebuildConfigFiles(UnixFile siteDirectory) {
+        // No configs to rebuild
+        return false;
+    }
+
+    protected boolean upgradeSiteDirectoryContents(UnixFile siteDirectory) throws IOException, SQLException {
+        // Nothing to do
+        return false;
+    }
+}
---
+++ src/com/aoindustries/aoserv/daemon/httpd/tomcat/TomcatCommon_7_0_X.java
@@ -0,0 +1,251 @@
+/*
+ * Copyright 2013 by AO Industries, Inc.,
+ * 7262 Bull Pen Cir, Mobile, Alabama, 36695, U.S.A.
+ * All rights reserved.
+ */
+package com.aoindustries.aoserv.daemon.httpd.tomcat;
+
+import com.aoindustries.aoserv.client.HttpdTomcatDataSource;
+import com.aoindustries.aoserv.daemon.OperatingSystemConfiguration;
+import com.aoindustries.aoserv.daemon.util.UpgradeSymlink;
+import com.aoindustries.io.ChainWriter;
+import com.aoindustries.io.unix.UnixFile;
+import java.io.IOException;
+import java.sql.SQLException;
+
+/**
+ * Some common code for Tomcat 7.0.X
+ *
+ * @author  AO Industries, Inc.
+ */
+class TomcatCommon_7_0_X extends TomcatCommon {
+
+    private static final TomcatCommon_7_0_X instance = new TomcatCommon_7_0_X();
+    static TomcatCommon_7_0_X getInstance() {
+        return instance;
+    }
+
+    private TomcatCommon_7_0_X() {}
+
+    public void writeHttpdTomcatDataSource(HttpdTomcatDataSource dataSource, ChainWriter out) throws SQLException, IOException {
+        out.print("          <Resource\n"
+                + "            name=\"").encodeXmlAttribute(dataSource.getName()).print("\"\n"
+                + "            auth=\"Container\"\n"
+                + "            type=\"javax.sql.DataSource\"\n"
+                + "            username=\"").encodeXmlAttribute(dataSource.getUsername()).print("\"\n"
+                + "            password=\"").encodeXmlAttribute(dataSource.getPassword()).print("\"\n"
+                + "            driverClassName=\"").encodeXmlAttribute(dataSource.getDriverClassName()).print("\"\n"
+                + "            url=\"").encodeXmlAttribute(dataSource.getUrl()).print("\"\n"
+                + "            maxActive=\"").print(dataSource.getMaxActive()).print("\"\n"
+                + "            maxIdle=\"").print(dataSource.getMaxIdle()).print("\"\n"
+                + "            maxWait=\"").print(dataSource.getMaxWait()).print("\"\n");
+        if(dataSource.getValidationQuery()!=null) {
+            out.print("            validationQuery=\"").encodeXmlAttribute(dataSource.getValidationQuery()).print("\"\n");
+        }
+        out.print("            removeAbandoned=\"true\"\n"
+                + "            removeAbandonedTimeout=\"300\"\n"
+                + "            logAbandoned=\"true\"\n"
+                + "          />\n");
+    }
+
+    /**
+     * Uses os-default JDK.
+     *
+     * @see  OperatingSystemConfiguration#getDefaultJdkVersion
+     */
+    public String getDefaultJdkVersion() throws IOException, SQLException {
+        return OperatingSystemConfiguration.getOperatingSystemConfiguration().getDefaultJdkVersion();
+    }
+
+	private static final UpgradeSymlink[] upgradeSymlinks_7_0 = {
+		// Upgrade from apache-tomcat-7
+		new UpgradeSymlink(
+			"bin/bootstrap.jar",
+			"../../../opt/apache-tomcat-7/bin/bootstrap.jar",
+			"../../../opt/apache-tomcat-7.0/bin/bootstrap.jar"
+		),
+		new UpgradeSymlink(
+			"bin/catalina.sh",
+			"../../../opt/apache-tomcat-7/bin/catalina.sh",
+			"../../../opt/apache-tomcat-7.0/bin/catalina.sh"
+		),
+		new UpgradeSymlink(
+			"bin/commons-daemon.jar",
+			"../../../opt/apache-tomcat-7/bin/commons-daemon.jar",
+			"../../../opt/apache-tomcat-7.0/bin/commons-daemon.jar"
+		),
+		new UpgradeSymlink(
+			"bin/digest.sh",
+			"../../../opt/apache-tomcat-7/bin/digest.sh",
+			"../../../opt/apache-tomcat-7.0/bin/digest.sh"
+		),
+		new UpgradeSymlink(
+			"bin/setclasspath.sh",
+			"../../../opt/apache-tomcat-7/bin/setclasspath.sh",
+			"../../../opt/apache-tomcat-7.0/bin/setclasspath.sh"
+		),
+		new UpgradeSymlink(
+			"bin/tomcat-juli.jar",
+			"../../../opt/apache-tomcat-7/bin/tomcat-juli.jar",
+			"../../../opt/apache-tomcat-7.0/bin/tomcat-juli.jar"
+		),
+		new UpgradeSymlink(
+			"bin/tool-wrapper.sh",
+			"../../../opt/apache-tomcat-7/bin/tool-wrapper.sh",
+			"../../../opt/apache-tomcat-7.0/bin/tool-wrapper.sh"
+		),
+		new UpgradeSymlink(
+			"bin/version.sh",
+			"../../../opt/apache-tomcat-7/bin/version.sh",
+			"../../../opt/apache-tomcat-7.0/bin/version.sh"
+		),
+		new UpgradeSymlink(
+			"lib/annotations-api.jar",
+			"../../../opt/apache-tomcat-7/lib/annotations-api.jar",
+			"../../../opt/apache-tomcat-7.0/lib/annotations-api.jar"
+		),
+		new UpgradeSymlink(
+			"lib/catalina-ant.jar",
+			"../../../opt/apache-tomcat-7/lib/catalina-ant.jar",
+			"../../../opt/apache-tomcat-7.0/lib/catalina-ant.jar"
+		),
+		new UpgradeSymlink(
+			"lib/catalina-ha.jar",
+			"../../../opt/apache-tomcat-7/lib/catalina-ha.jar",
+			"../../../opt/apache-tomcat-7.0/lib/catalina-ha.jar"
+		),
+		new UpgradeSymlink(
+			"lib/catalina.jar",
+			"../../../opt/apache-tomcat-7/lib/catalina.jar",
+			"../../../opt/apache-tomcat-7.0/lib/catalina.jar"
+		),
+		new UpgradeSymlink(
+			"lib/catalina-tribes.jar",
+			"../../../opt/apache-tomcat-7/lib/catalina-tribes.jar",
+			"../../../opt/apache-tomcat-7.0/lib/catalina-tribes.jar"
+		),
+		new UpgradeSymlink(
+			"lib/ecj-3.7.1.jar",
+			"../../../opt/apache-tomcat-7/lib/ecj-3.7.1.jar",
+			null
+		),
+		new UpgradeSymlink(
+			"lib/el-api.jar",
+			"../../../opt/apache-tomcat-7/lib/el-api.jar",
+			"../../../opt/apache-tomcat-7.0/lib/el-api.jar"
+		),
+		new UpgradeSymlink(
+			"lib/jasper-el.jar",
+			"../../../opt/apache-tomcat-7/lib/jasper-el.jar",
+			"../../../opt/apache-tomcat-7.0/lib/jasper-el.jar"
+		),
+		new UpgradeSymlink(
+			"lib/jasper.jar",
+			"../../../opt/apache-tomcat-7/lib/jasper.jar",
+			"../../../opt/apache-tomcat-7.0/lib/jasper.jar"
+		),
+		new UpgradeSymlink(
+			"lib/jsp-api.jar",
+			"../../../opt/apache-tomcat-7/lib/jsp-api.jar",
+			"../../../opt/apache-tomcat-7.0/lib/jsp-api.jar"
+		),
+		new UpgradeSymlink(
+			"lib/servlet-api.jar",
+			"../../../opt/apache-tomcat-7/lib/servlet-api.jar",
+			"../../../opt/apache-tomcat-7.0/lib/servlet-api.jar"
+		),
+		new UpgradeSymlink(
+			"lib/tomcat-api.jar",
+			"../../../opt/apache-tomcat-7/lib/tomcat-api.jar",
+			"../../../opt/apache-tomcat-7.0/lib/tomcat-api.jar"
+		),
+		new UpgradeSymlink(
+			"lib/tomcat-coyote.jar",
+			"../../../opt/apache-tomcat-7/lib/tomcat-coyote.jar",
+			"../../../opt/apache-tomcat-7.0/lib/tomcat-coyote.jar"
+		),
+		new UpgradeSymlink(
+			"lib/tomcat-dbcp.jar",
+			"../../../opt/apache-tomcat-7/lib/tomcat-dbcp.jar",
+			"../../../opt/apache-tomcat-7.0/lib/tomcat-dbcp.jar"
+		),
+		new UpgradeSymlink(
+			"lib/tomcat-i18n-es.jar",
+			"../../../opt/apache-tomcat-7/lib/tomcat-i18n-es.jar",
+			"../../../opt/apache-tomcat-7.0/lib/tomcat-i18n-es.jar"
+		),
+		new UpgradeSymlink(
+			"lib/tomcat-i18n-fr.jar",
+			"../../../opt/apache-tomcat-7/lib/tomcat-i18n-fr.jar",
+			"../../../opt/apache-tomcat-7.0/lib/tomcat-i18n-fr.jar"
+		),
+		new UpgradeSymlink(
+			"lib/tomcat-i18n-ja.jar",
+			"../../../opt/apache-tomcat-7/lib/tomcat-i18n-ja.jar",
+			"../../../opt/apache-tomcat-7.0/lib/tomcat-i18n-ja.jar"
+		),
+		new UpgradeSymlink(
+			"lib/tomcat-jdbc.jar",
+			"../../../opt/apache-tomcat-7/lib/tomcat-jdbc.jar",
+			"../../../opt/apache-tomcat-7.0/lib/tomcat-jdbc.jar"
+		),
+		new UpgradeSymlink(
+			"lib/tomcat-util.jar",
+			"../../../opt/apache-tomcat-7/lib/tomcat-util.jar",
+			"../../../opt/apache-tomcat-7.0/lib/tomcat-util.jar"
+		),
+		// New links for current Tomcat version
+		new UpgradeSymlink(
+			"lib/ecj-4.2.2.jar",
+			null,
+			"../../../opt/apache-tomcat-7.0/lib/ecj-4.2.2.jar"
+		)
+	};
+
+	/**
+	 * Upgrades the Tomcat 6.0.X installed in the provided directory.
+	 */
+	boolean upgradeTomcatDirectory(UnixFile tomcatDirectory, int uid, int gid) throws IOException, SQLException {
+		boolean needsRestart = false;
+		OperatingSystemConfiguration osConfig = OperatingSystemConfiguration.getOperatingSystemConfiguration();
+		if(osConfig==OperatingSystemConfiguration.CENTOS_5_I686_AND_X86_64) {
+			// Tomcat 6.0
+			for(UpgradeSymlink upgradeSymlink : upgradeSymlinks_7_0) {
+				if(upgradeSymlink.upgradeLinkTarget(tomcatDirectory, uid, gid)) needsRestart = true;
+			}
+			// MySQL
+			for(UpgradeSymlink upgradeSymlink : upgradeSymlinks_MySQL) {
+				if(upgradeSymlink.upgradeLinkTarget(tomcatDirectory, uid, gid)) needsRestart = true;
+			}
+			// PostgreSQL
+			for(UpgradeSymlink upgradeSymlink : upgradeSymlinks_PostgreSQL) {
+				if(upgradeSymlink.upgradeLinkTarget(tomcatDirectory, uid, gid)) needsRestart = true;
+			}
+			// Update bin/profile
+			// TODO
+			/*
+			String results = AOServDaemon.execAndCapture(
+				new String[] {
+					osConfig.getReplaceCommand(),
+					"/aoserv.sh",
+					"/aoserv-client.sh",
+					"php-4.3.sh",
+					"php-4.sh",
+					"php-4.3.3.sh",
+					"php-4.sh",
+					"php-4.3.8.sh",
+					"php-4.sh",
+					"postgresql-7.3.3.sh",
+					"postgresql-7.3.sh",
+					"export \"CLASSPATH=/usr/aoserv/lib-1.3/aocode-public.jar:$CLASSPATH\".d/001_aoserv.sh",
+					"export \"CLASSPATH=/opt/aoserv-client/lib-1.3/aocode-public.jar:$CLASSPATH\"",
+					"--",
+					tomcatDirectory.getPath()+"/bin/profile"
+				}
+			);
+			if(results.length()>0) needsRestart = true;
+			 */
+		}
+		return needsRestart;
+	}
+}
