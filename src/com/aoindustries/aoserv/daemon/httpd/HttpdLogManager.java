578bb4ac9e369b5f89493fb9c42c462ee5c5bcff
Dan Armstrong <dan@aoindustries.com>
2017-02-27 20:54:28 +0000

    Working on CentOS 7 compatibility.

--- src/com/aoindustries/aoserv/daemon/httpd/HttpdLogManager.java
+++ src/com/aoindustries/aoserv/daemon/httpd/HttpdLogManager.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2008-2013, 2014, 2015, 2016 by AO Industries, Inc.,
+ * Copyright 2008-2013, 2014, 2015, 2016, 2017 by AO Industries, Inc.,
  * 7262 Bull Pen Cir, Mobile, Alabama, 36695, U.S.A.
  * All rights reserved.
  */
@@ -23,6 +23,7 @@ import java.sql.SQLException;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
+import java.util.regex.Pattern;
 
 /**
  * Manages web site logs.
@@ -33,15 +34,35 @@ class HttpdLogManager {
 
 	/**
 	 * The directory that logs are stored in.
+	 *
+	 * TODO: CentOS 7: This goes somewhere else... and make symlinks from /var/www/... directories.
+	 *                 Maybe /var/log/www/site-name?
 	 */
-	static final String LOG_DIR="/logs";
+	static final String LOG_DIR = "/logs";
 
 	/**
 	 * The directory that contains the log rotation scripts.
 	 */
-	static final String LOG_ROTATION_DIR_REDHAT = HttpdServerManager.CONF_DIRECTORY + "/logrotate.d";
-	static final String LOG_ROTATION_DIR_CENTOS = HttpdServerManager.CONF_DIRECTORY + "/logrotate.sites";
-	static final String SERVER_LOG_ROTATION_DIR_CENTOS = HttpdServerManager.CONF_DIRECTORY + "/logrotate.servers";
+	private static final String LOG_ROTATION_DIR_REDHAT = HttpdServerManager.CONF_DIRECTORY + "/logrotate.d";
+	private static final String LOG_ROTATION_DIR_CENTOS_5 = HttpdServerManager.CONF_DIRECTORY + "/logrotate.sites";
+	private static final String SERVER_LOG_ROTATION_DIR_CENTOS_5 = HttpdServerManager.CONF_DIRECTORY + "/logrotate.servers";
+	// CentOS 7: we're putting all log rotations directly in /etc/logrotate.d to avoid required modification of /etc/logrotate.conf
+	private static final String ETC_LOGROTATE_D = "/etc/logrotate.d";
+
+	/**
+	 * Logrotate file prefix used for HttpdSite in /etc/logrotate.d
+	 */
+	private static final String HTTPD_SITE_PREFIX = "httpd-site-";
+
+	/**
+	 * Logrotate file prefix used for HttpdServer
+	 */
+	private static final String HTTPD_SERVER_PREFIX = "httpd";
+
+	/**
+	 * Pattern to match HttpdServer when in /etc/logrotate.d
+	 */
+	private static final Pattern HTTPD_SERVER_REGEXP = Pattern.compile("^" + HTTPD_SERVER_PREFIX + "[0-9]+$");
 
 	/**
 	 * The directory that contains the per-apache-instance logs.
@@ -64,14 +85,11 @@ class HttpdLogManager {
 		// Rebuild /logs
 		doRebuildLogs(aoServer, deleteFileList, serversNeedingReloaded);
 
-		// Rebuild /etc/httpd/conf/logrotate.(d|sites|servers) files
+		// Rebuild /etc/logrotate.d or /etc/httpd/conf/logrotate.(d|sites|servers) files
 		doRebuildLogrotate(aoServer, deleteFileList, bout);
 
 		// Rebuild /var/log/httpd
 		doRebuildVarLogHttpd(aoServer, deleteFileList);
-
-		// Other filesystem fixes related to logging
-		fixFilesystem(deleteFileList);
 	}
 
 	/**
@@ -102,37 +120,6 @@ class HttpdLogManager {
 				if(!dontDeleteFilenames.contains(filename)) {
 					if(!filename.startsWith("suexec.log")) deleteFileList.add(new UnixFile(serverLogDir, filename, false).getFile());
 				}
-			}
-		}
-	}
-
-	private static final UnixFile[] centOsAlwaysDelete = {
-		new UnixFile("/etc/logrotate.d/httpd"),
-		new UnixFile("/etc/logrotate.d/httpd1"),
-		new UnixFile("/etc/logrotate.d/httpd2"),
-		new UnixFile("/etc/logrotate.d/httpd3"),
-		new UnixFile("/etc/logrotate.d/httpd4"),
-		new UnixFile("/etc/logrotate.d/httpd5"),
-		new UnixFile("/etc/logrotate.d/httpd6"),
-		new UnixFile("/etc/httpd/conf/logrotate.d"),
-		new UnixFile("/opt/aoserv-daemon/logrotate.d/httpd1"),
-		new UnixFile("/opt/aoserv-daemon/logrotate.d/httpd2"),
-		new UnixFile("/opt/aoserv-daemon/logrotate.d/httpd3"),
-		new UnixFile("/opt/aoserv-daemon/logrotate.d/httpd4"),
-		new UnixFile("/opt/aoserv-daemon/logrotate.d/httpd5"),
-		new UnixFile("/opt/aoserv-daemon/logrotate.d/httpd6")
-	};
-
-	/**
-	 * Fixes any filesystem stuff related to logging.
-	 */
-	private static void fixFilesystem(List<File> deleteFileList) throws IOException, SQLException {
-		HttpdOperatingSystemConfiguration osConfig = HttpdOperatingSystemConfiguration.getHttpOperatingSystemConfiguration();
-		if(osConfig==HttpdOperatingSystemConfiguration.CENTOS_5_I686_AND_X86_64) {
-			// Make sure these files don't exist.  They may be due to upgrades or a
-			// result of RPM installs.
-			for(UnixFile uf : centOsAlwaysDelete) {
-				if(uf.getStat().exists()) deleteFileList.add(uf.getFile());
 			}
 		}
 	}
@@ -233,14 +220,22 @@ class HttpdLogManager {
 		final HttpdOperatingSystemConfiguration osConfig = HttpdOperatingSystemConfiguration.getHttpOperatingSystemConfiguration();
 		final String siteLogRotationDir;
 		final String serverLogRotationDir;
+		final boolean isGlobalLogrotateDir;
 		switch(osConfig) {
 			case REDHAT_ES_4_X86_64 :
 				siteLogRotationDir = LOG_ROTATION_DIR_REDHAT;
 				serverLogRotationDir = null;
+				isGlobalLogrotateDir = false;
 				break;
 			case CENTOS_5_I686_AND_X86_64 :
-				siteLogRotationDir = LOG_ROTATION_DIR_CENTOS;
-				serverLogRotationDir = SERVER_LOG_ROTATION_DIR_CENTOS;
+				siteLogRotationDir = LOG_ROTATION_DIR_CENTOS_5;
+				serverLogRotationDir = SERVER_LOG_ROTATION_DIR_CENTOS_5;
+				isGlobalLogrotateDir = false;
+				break;
+			case CENTOS_7_X86_64 :
+				siteLogRotationDir = ETC_LOGROTATE_D;
+				serverLogRotationDir = ETC_LOGROTATE_D;
+				isGlobalLogrotateDir = true;
 				break;
 			default :
 				throw new AssertionError("Unexpected value for osConfig: "+osConfig);
@@ -250,12 +245,21 @@ class HttpdLogManager {
 		int gid_min = thisAoServer.getGidMin().getID();
 
 		// Create directory if missing
-		DaemonFileUtils.mkdir(siteLogRotationDir, 0700, UnixFile.ROOT_UID, UnixFile.ROOT_GID);
+		if(!isGlobalLogrotateDir) {
+			DaemonFileUtils.mkdir(siteLogRotationDir, 0700, UnixFile.ROOT_UID, UnixFile.ROOT_GID);
+		}
 
 		// The log rotations that exist but are not used will be removed
 		String[] list = new File(siteLogRotationDir).list();
 		Set<String> logRotationFiles = new HashSet<>(list.length*4/3+1);
-		for(String filename : list) logRotationFiles.add(filename);
+		for(String filename : list) {
+			if(
+				!isGlobalLogrotateDir
+				|| filename.startsWith(HTTPD_SITE_PREFIX)
+			) {
+				logRotationFiles.add(filename);
+			}
+		}
 
 		// Each log file will be only rotated at most once
 		Set<String> completedPaths = new HashSet<>(list.length*4/3+1);
@@ -266,10 +270,7 @@ class HttpdLogManager {
 			// Write the new file to RAM first
 			byteOut.reset();
 			boolean wroteOne = false;
-			List<HttpdSiteBind> binds=site.getHttpdSiteBinds();
-
-			for(int d=0;d<binds.size();d++) {
-				HttpdSiteBind bind=binds.get(d);
+			for(HttpdSiteBind bind : site.getHttpdSiteBinds()) {
 				String access_log = bind.getAccessLog();
 				// Each unique path is only rotated once
 				if(completedPaths.add(access_log)) {
@@ -286,48 +287,59 @@ class HttpdLogManager {
 					chainOut.print(error_log);
 				}
 			}
-			// Finish the file
+			// Do not write empty files, finish the file
 			if(wroteOne) {
 				chainOut.print(" {\n"
 							 + "    missingok\n"
 							 + "    daily\n"
 							 + "    rotate 379\n"
 							 + "}\n");
+				chainOut.flush();
+				byte[] newFileContent=byteOut.toByteArray();
+
+				// Write to disk if file missing or doesn't match
+				DaemonFileUtils.writeIfNeeded(
+					newFileContent,
+					null,
+					new UnixFile(siteLogRotationDir, site.getSiteName()),
+					UnixFile.ROOT_UID,
+					site.getLinuxServerGroup().getGid().getID(),
+					0640,
+					uid_min,
+					gid_min
+				);
+
+				// Make sure the newly created or replaced log rotation file is not removed
+				logRotationFiles.remove(site.getSiteName());
 			}
-			chainOut.flush();
-			byte[] newFileContent=byteOut.toByteArray();
-
-			// Write to disk if file missing or doesn't match
-			DaemonFileUtils.writeIfNeeded(
-				newFileContent,
-				null,
-				new UnixFile(siteLogRotationDir, site.getSiteName()),
-				UnixFile.ROOT_UID,
-				site.getLinuxServerGroup().getGid().getID(),
-				0640,
-				uid_min,
-				gid_min
-			);
-
-			// Make sure the newly created or replaced log rotation file is not removed
-			logRotationFiles.remove(site.getSiteName());
 		}
 
 		// Remove extra filenames
-		for(String extraFilename : logRotationFiles) deleteFileList.add(new File(siteLogRotationDir, extraFilename));
+		for(String extraFilename : logRotationFiles) {
+			deleteFileList.add(new File(siteLogRotationDir, extraFilename));
+		}
 
-		if(serverLogRotationDir!=null) {
+		if(serverLogRotationDir != null) {
 			// Create directory if missing
-			DaemonFileUtils.mkdir(serverLogRotationDir, 0700, UnixFile.ROOT_UID, UnixFile.ROOT_GID);
+			if(!isGlobalLogrotateDir) {
+				DaemonFileUtils.mkdir(serverLogRotationDir, 0700, UnixFile.ROOT_UID, UnixFile.ROOT_GID);
+			}
 
 			// The log rotations that exist but are not used will be removed
 			logRotationFiles.clear();
-			for(String filename : new File(serverLogRotationDir).list()) logRotationFiles.add(filename);
+			for(String filename : new File(serverLogRotationDir).list()) {
+				if(
+					!isGlobalLogrotateDir
+					|| HTTPD_SERVER_REGEXP.matcher(filename).matches()
+				) {
+					logRotationFiles.add(filename);
+				}
+			}
 
 			boolean isFirst = true;
 			for(HttpdServer hs : thisAoServer.getHttpdServers()) {
 				int num = hs.getNumber();
-				String filename = "httpd"+num;
+				String filename = HTTPD_SERVER_PREFIX + num;
 				logRotationFiles.remove(filename);
 
 				// Build to RAM first
@@ -385,7 +397,9 @@ class HttpdLogManager {
 			}
 
 			// Remove extra filenames
-			for(String extraFilename : logRotationFiles) deleteFileList.add(new File(serverLogRotationDir, extraFilename));
+			for(String extraFilename : logRotationFiles) {
+				deleteFileList.add(new File(serverLogRotationDir, extraFilename));
+			}
 		}
 	}
 
