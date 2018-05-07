f93c39def72a031971dce75530e5175ccbd486fa
Dan Armstrong <dan@aoindustries.com>
2018-05-07 04:37:44 +0000

    Rebuilding when firewalld_zones or net_bind_firewalld_zones updated, too.

--- src/com/aoindustries/aoserv/daemon/net/firewalld/FirewalldManager.java
+++ src/com/aoindustries/aoserv/daemon/net/firewalld/FirewalldManager.java
@@ -581,7 +581,9 @@ final public class FirewalldManager extends BuilderThread {
 				if(osvId == OperatingSystemVersion.CENTOS_7_X86_64) {
 					AOServConnector conn = AOServDaemon.getConnector();
 					firewalldManager = new FirewalldManager();
+					conn.getFirewalldZones().addTableListener(firewalldManager, 0);
 					conn.getNetBinds().addTableListener(firewalldManager, 0);
+					conn.getNetBindFirewalldZones().addTableListener(firewalldManager, 0);
 					PackageManager.addPackageListener(firewalldManager);
 					System.out.println("Done");
 				} else {
