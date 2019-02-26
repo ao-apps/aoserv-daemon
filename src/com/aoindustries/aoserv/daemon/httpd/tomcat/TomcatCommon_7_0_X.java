47fda10bfc105ac0b211c3338d24035134e59075
Dan Armstrong <dan@aoindustries.com>
2019-02-26 18:44:26 +0000

    Tomcat 7.0.93 support.

--- src/com/aoindustries/aoserv/daemon/httpd/tomcat/TomcatCommon_7_0_X.java
+++ src/com/aoindustries/aoserv/daemon/httpd/tomcat/TomcatCommon_7_0_X.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2013, 2015, 2016, 2017, 2018 by AO Industries, Inc.,
+ * Copyright 2013, 2015, 2016, 2017, 2018, 2019 by AO Industries, Inc.,
  * 7262 Bull Pen Cir, Mobile, Alabama, 36695, U.S.A.
  * All rights reserved.
  */
@@ -298,6 +298,7 @@ class TomcatCommon_7_0_X extends TomcatCommon {
 			} else if(
 				rpmVersion.equals("7.0.91")
 				|| rpmVersion.equals("7.0.92")
+				|| rpmVersion.equals("7.0.93")
 			) {
 				UpgradeSymlink[] upgradeSymlinks_7_0_91 = {
 					new UpgradeSymlink(
