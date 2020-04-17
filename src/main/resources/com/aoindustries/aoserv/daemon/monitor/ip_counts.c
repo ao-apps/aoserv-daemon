fa5ff6a580b4d5dd26b5933bcfcb326e6f54c061
AO Industries, Inc. <support@aoindustries.com>
2020-04-16 23:58:05 -0500

    Converted top comment to LGPLv3 style.

--- src/main/resources/com/aoindustries/aoserv/daemon/monitor/ip_counts.c
+++ src/main/resources/com/aoindustries/aoserv/daemon/monitor/ip_counts.c
@@ -1,3 +1,26 @@
+//
+// aoserv-daemon - Server management daemon for the AOServ Platform.
+// Copyright (C) 2013, 2020  AO Industries, Inc.
+//     support@aoindustries.com
+//     7262 Bull Pen Cir
+//     Mobile, AL 36695
+//
+// This file is part of aoserv-daemon.
+//
+// aoserv-daemon is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// aoserv-daemon is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with aoserv-daemon.  If not, see <http://www.gnu.org/licenses/>.
+//
+
 // Compile with:
 // gcc -O3 -Winline --param large-function-growth=500 --param inline-unit-growth=150 --param max-inline-insns-single=700 -lpcap -o ../bin/ip_counts ip_counts.c
 
