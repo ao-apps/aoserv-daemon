65e5bc3c48855a7b1d331b6a1e75341344b0c064
AO Industries, Inc <support@aoindustries.com>
2020-12-30 05:52:58 -0600

    Fixed: PostgreSQL JDBC is 42.2.18, not 42.2.17

--- src/main/java/com/aoindustries/aoserv/daemon/httpd/tomcat/TomcatCommon_7_0_X.java
+++ src/main/java/com/aoindustries/aoserv/daemon/httpd/tomcat/TomcatCommon_7_0_X.java
@@ -602,6 +602,23 @@ class TomcatCommon_7_0_X extends TomcatCommon {
 						null,
 						"../" + optSlash + "apache-tomcat-7.0/lib/postgresql-42.2.17.jre6.jar"
 					),
+					// postgresql-42.2.17.jre6.jar -> postgresql-42.2.18.jre6.jar
+					new UpgradeSymlink(
+						"lib/postgresql-42.2.17.jre6.jar",
+						"/dev/null",
+						"lib/postgresql-42.2.18.jre6.jar",
+						"/dev/null"
+					),
+					new UpgradeSymlink(
+						"lib/postgresql-42.2.17.jre6.jar",
+						"../" + optSlash + "apache-tomcat-7.0/lib/postgresql-42.2.17.jre6.jar",
+						null
+					),
+					new UpgradeSymlink(
+						"lib/postgresql-42.2.18.jre6.jar",
+						null,
+						"../" + optSlash + "apache-tomcat-7.0/lib/postgresql-42.2.18.jre6.jar"
+					),
 				};
 				for(UpgradeSymlink upgradeSymlink : upgradeSymlinks_7_0_106) {
 					if(upgradeSymlink.upgradeLinkTarget(tomcatDirectory, uid, gid)) needsRestart = true;
