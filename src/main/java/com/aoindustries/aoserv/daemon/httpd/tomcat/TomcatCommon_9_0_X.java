21274ccf76cae12b4538889a086e07fb646dacaa
AO Industries, Inc <support@aoindustries.com>
2022-12-06 23:38:41 -0600

    Updated to apache-tomcat-9.0.70

--- src/main/java/com/aoindustries/aoserv/daemon/httpd/tomcat/TomcatCommon_9_0_X.java
+++ src/main/java/com/aoindustries/aoserv/daemon/httpd/tomcat/TomcatCommon_9_0_X.java
@@ -174,6 +174,9 @@ final class TomcatCommon_9_0_X extends VersionedTomcatCommon {
       Version version = new Version(rpm.getVersion(), rpm.getRelease());
       String suffix = osConfig.getPackageReleaseSuffix();
       // Downgrade support
+      if (version.compareTo("9.0.70-1" + suffix) < 0) {
+        // 9.0.70-1 has same files as 9.0.69-2
+      }
       if (version.compareTo("9.0.69-2" + suffix) < 0) {
         UpgradeSymlink[] downgradeSymlinks = {
             // postgresql-42.5.1.jar -> postgresql-42.5.0.jar
@@ -1100,7 +1103,10 @@ final class TomcatCommon_9_0_X extends VersionedTomcatCommon {
           }
         }
       }
-      if (version.compareTo("9.0.69-2" + suffix) > 0) {
+      if (version.compareTo("9.0.70-1" + suffix) >= 0) {
+        // 9.0.70-1 has same files as 9.0.69-2
+      }
+      if (version.compareTo("9.0.70-1" + suffix) > 0) {
         throw new IllegalStateException("Version of Tomcat newer than expected: " + version);
       }
     }
