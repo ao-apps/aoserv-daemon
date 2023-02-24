decf0fbd8e554a2d9ec3da752650557dffadeb8b
AO Industries, Inc <support@aoindustries.com>
2023-02-24 15:29:28 -0600

    CentOS packages are at AORepo.org

--- src/main/java/com/aoindustries/aoserv/daemon/OperatingSystemConfiguration.java
+++ src/main/java/com/aoindustries/aoserv/daemon/OperatingSystemConfiguration.java
@@ -1,6 +1,6 @@
 /*
  * aoserv-daemon - Server management daemon for the AOServ Platform.
- * Copyright (C) 2008-2013, 2016, 2017, 2018, 2019, 2020, 2021, 2022  AO Industries, Inc.
+ * Copyright (C) 2008-2013, 2016, 2017, 2018, 2019, 2020, 2021, 2022, 2023  AO Industries, Inc.
  *     support@aoindustries.com
  *     7262 Bull Pen Cir
  *     Mobile, AL 36695
@@ -123,7 +123,7 @@ public enum OperatingSystemConfiguration {
 
     @Override
     public String getPackageReleaseSuffix() {
-      return ".ao";
+      return ".aorepo";
     }
   },
   CENTOS_5DOM0_I686 {
@@ -329,7 +329,7 @@ public enum OperatingSystemConfiguration {
 
     @Override
     public String getPackageReleaseSuffix() {
-      return ".el7.ao";
+      return ".el7.aorepo";
     }
   },
   CENTOS_7DOM0_X86_64 {
