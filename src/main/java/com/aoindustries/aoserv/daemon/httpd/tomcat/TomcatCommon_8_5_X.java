e5fedcb272a12b87d64d4be64ce43b9e26f83df9
AO Industries, Inc <support@aoindustries.com>
2023-10-25 02:01:32 -0500

    Tomcat 8.5.95 and 10.1.15

--- src/main/java/com/aoindustries/aoserv/daemon/httpd/tomcat/TomcatCommon_8_5_X.java
+++ src/main/java/com/aoindustries/aoserv/daemon/httpd/tomcat/TomcatCommon_8_5_X.java
@@ -181,6 +181,9 @@ final class TomcatCommon_8_5_X extends VersionedTomcatCommon {
       final Version version = new Version(rpm.getVersion(), rpm.getRelease());
       final String suffix = osConfig.getPackageReleaseSuffix();
       // Downgrade support
+      if (version.compareTo("8.5.95-1" + suffix) < 0) {
+        // 8.5.95-1 has same files as 8.5.94-1
+      }
       if (version.compareTo("8.5.94-1" + suffix) < 0) {
         // 8.5.94-1 has same files as 8.5.93-1
       }
@@ -1228,7 +1231,10 @@ final class TomcatCommon_8_5_X extends VersionedTomcatCommon {
       if (version.compareTo("8.5.94-1" + suffix) >= 0) {
         // 8.5.94-1 has same files as 8.5.93-1
       }
-      if (version.compareTo("8.5.94-1" + suffix) > 0) {
+      if (version.compareTo("8.5.95-1" + suffix) >= 0) {
+        // 8.5.95-1 has same files as 8.5.94-1
+      }
+      if (version.compareTo("8.5.95-1" + suffix) > 0) {
         throw new IllegalStateException("Version of Tomcat newer than expected: " + version);
       }
     }
