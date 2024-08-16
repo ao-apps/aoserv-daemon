9f76625aab34ed223c64c0ef1f7f3aebc8bbd492
AO Industries, Inc <support@aoindustries.com>
2024-08-16 13:04:45 -0500

    CvsManager not supported in Rocky 9

--- src/main/java/com/aoindustries/aoserv/daemon/cvsd/CvsManager.java
+++ src/main/java/com/aoindustries/aoserv/daemon/cvsd/CvsManager.java
@@ -1,6 +1,6 @@
 /*
  * aoserv-daemon - Server management daemon for the AOServ Platform.
- * Copyright (C) 2002-2013, 2015, 2016, 2017, 2018, 2019, 2020, 2021, 2022  AO Industries, Inc.
+ * Copyright (C) 2002-2013, 2015, 2016, 2017, 2018, 2019, 2020, 2021, 2022, 2024  AO Industries, Inc.
  *     support@aoindustries.com
  *     7262 Bull Pen Cir
  *     Mobile, AL 36695
@@ -75,6 +75,7 @@ public final class CvsManager extends BuilderThread {
       if (
           osvId != OperatingSystemVersion.CENTOS_5_I686_AND_X86_64
               && osvId != OperatingSystemVersion.CENTOS_7_X86_64
+              && osvId != OperatingSystemVersion.ROCKY_9_X86_64
       ) {
         throw new AssertionError("Unsupported OperatingSystemVersion: " + osv);
       }
@@ -88,6 +89,7 @@ public final class CvsManager extends BuilderThread {
                 && (
                 osvId == OperatingSystemVersion.CENTOS_5_I686_AND_X86_64
                     || osvId == OperatingSystemVersion.CENTOS_7_X86_64
+                    || osvId == OperatingSystemVersion.ROCKY_9_X86_64
             )
         ) {
           // Install any required RPMs
@@ -206,6 +208,7 @@ public final class CvsManager extends BuilderThread {
         if (
             osvId == OperatingSystemVersion.CENTOS_5_I686_AND_X86_64
                 || osvId == OperatingSystemVersion.CENTOS_7_X86_64
+                || osvId == OperatingSystemVersion.ROCKY_9_X86_64
         ) {
           AoservConnector conn = AoservDaemon.getConnector();
           cvsManager = new CvsManager();
